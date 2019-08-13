94c324399f88d6295b311b47a1525d61330e94b9
Xaver <xaverfischer@mail.de>
2019-08-13 02:02:51 +0200

    print adds a newline

--- lib/intrinsics.cpp
+++ lib/intrinsics.cpp
@@ -1,6 +1,6 @@
 #include<cstdio>
 #define EXPORT __attribute__((visibility("default")))
 extern "C" double print(double x) {
-    fprintf(stdout, "%f",x);
+    fprintf(stdout, "%f\n",x);
     return 0;
 }
